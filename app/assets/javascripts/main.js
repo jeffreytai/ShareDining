5000d2e59cabf15e49e03ab5875efa6f1a24b1f2
Mahir Shah <mahir@mahirshah.com>
2016-04-20 15:38:17 -0700

    added modals for signup and login with working js/css

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,14 +1,17 @@
 import GoogleMap from 'GoogleMap';
-import ModalShow from 'ModalShow';
+import ModalController from 'ModalController';
 
 // TODO: change this to real data, need to get lat/long based on value of search
 const mapElName = '.results-map-container';
 const landingLoginButton = '.nav-login-button';
+const landingSignupButton = '.nav-signup-button';
 
 window.map = new GoogleMap(mapElName, { lat: 45.5231, lng: -122.6765 });
 
 document.addEventListener('DOMContentLoaded', () => {
-  const modalController = new ModalShow({
-    'login-show': [document.querySelector(landingLoginButton)]
-  }).initBindings();
+  const $loginLandingButton = document.querySelector(landingLoginButton);
+  const $landingSignupButton = document.querySelector(landingSignupButton);
+
+  const loginModalController = new ModalController('login-modal-show', [$loginLandingButton], 'login-modal').init();
+  const signupModalController = new ModalController('signup-modal-show', [$landingSignupButton], 'signup-modal').init();
 });
