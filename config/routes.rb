52c3096897ed1dbd89403c224f08c7c9f2c13a1a
Jeffrey Tai <jeffreytai@ucla.edu>
2016-04-19 22:20:27 -0700

    added first and last name to user registration, moved menu bar to shared/menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
-  get 'search_results/results'
-
   devise_for :users
+
   root 'landing#user'
+
+  get 'search_results/results'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
