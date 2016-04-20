0bf51f0c12ef20b332c3eb68eb4af3f99e18fa57
Mahir Shah <mahir@mahirshah.com>
2016-04-20 02:02:42 -0700

    added login modal, not completely done

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,5 +1,14 @@
 import GoogleMap from 'GoogleMap';
+import ModalShow from 'ModalShow';
 
 // TODO: change this to real data, need to get lat/long based on value of search
 const mapElName = '.results-map-container';
+const landingLoginButton = '.nav-login-button';
+
 window.map = new GoogleMap(mapElName, { lat: 45.5231, lng: -122.6765 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const modalController = new ModalShow({
+    'login-show': [document.querySelector(landingLoginButton)]
+  }).initBindings();
+});
