a1ccb89bacbdd3748554261fa3496d772c2757a8
Mahir Shah <mahir@mahirshah.com>
2016-04-20 18:49:33 -0700

    made modals accessible

--- app/assets/javascripts/ModalController.js
+++ app/assets/javascripts/ModalController.js
@@ -9,19 +9,40 @@ export default class ModalShow {
     const $body = document.querySelector('body');
     const $modal = document.querySelector(`.${this.modalClass}`);
     const ESCAPE_KEY_CODE = 27;
+    let lastFocus = null;
+    const closeModal = () => {
+      $body.classList.remove(this.bodyModalClass);
+      lastFocus.focus(); // restore focus to the old element
+    };
 
     // each link should open the modal by adding the bodyModalClass to body
     this.modalLinks.forEach((link) => {
       link.addEventListener('click', (evt) => {
+        // save the last focus
+        lastFocus = document.activeElement;
         $body.classList.add(this.bodyModalClass);
-        evt.stopPropagation(); // stop prop so we don't close the modal when we bubble up to body
+
+        // focus on the modal
+        $modal.setAttribute('tabindex', 0);
+        $modal.focus();
+
+        // stop prop so we don't close the modal when we bubble up to body
+        evt.stopPropagation();
       });
     });
+
+    // make sure only modal elements are focusable
+    document.addEventListener('focus', (evt) => {
+      if ($body.classList.contains(this.bodyModalClass) && !$modal.contains(evt.target)) {
+        event.preventDefault();
+        $modal.focus();
+      }
+    }, true);
 
     // a click outside the modal should close it
     $body.addEventListener('click', () => {
       if ($body.classList.contains(this.bodyModalClass)) {
-        $body.classList.remove(this.bodyModalClass);
+        closeModal();
       }
     });
 
@@ -31,7 +52,7 @@ export default class ModalShow {
     // handle modal close on escape
     document.addEventListener('keyup', (evt) => {
       if (evt.keyCode === ESCAPE_KEY_CODE && $body.classList.contains(this.bodyModalClass)) {
-        $body.classList.remove(this.bodyModalClass);
+        closeModal();
       }
     });
   }
