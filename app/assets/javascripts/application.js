426249587dca54ae62404cc10b3ddc01c4d6a5be
Jeffrey Tai <jeffreytai@ucla.edu>
2016-04-21 00:16:46 -0700

    removed up/down arrows in date, added gem for eventual games autocomplete functionality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-// = require turbolinks
+//= require turbolinks
+//= require jquery_ujs
+//= require jquery-ui
+//- require gmaps-auto-complete
 
 require('./main');
-
