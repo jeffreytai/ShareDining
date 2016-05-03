c8fd581c21ed92e94642e33ed5e6a5634985336a
Mahir Shah <mahir@mahirshah.com>
2016-05-03 00:09:27 -0700

    added completion and incomplete indicators for inventory form

--- app/assets/javascripts/pageJs/kitchenNewController.js
+++ app/assets/javascripts/pageJs/kitchenNewController.js
@@ -7,7 +7,8 @@ jsController['kitchen-new'] = function () {
   const MENU_LINK_CLASS = 'kitchen-new-menu-link';
   const STICKY_MENU_CLASS = 'stuck';
   const KITCHEN_MENU_LINK_CLASS = 'kitchen-new-menu-link';
-
+  const COMPLETED_LINK_CLASS = 'completed';
+  const INCOMPLETE_LINK_CLASS = 'incomplete';
   const navHeight = document.querySelector('.main-nav').offsetHeight;
   const $stickyMenu = document.querySelector('.kitchen-new-side-menu');
   const $body = document.querySelector('body');
@@ -15,7 +16,7 @@ jsController['kitchen-new'] = function () {
   const $nextButton = document.querySelector('.next-button');
   const $prevButton = document.querySelector('.prev-button');
 
-  let $currentFieldset = document.querySelector('.fieldset.current'),
+  var $currentFieldset = document.querySelector('.fieldset.current'),
     $currentMenuItem = document.querySelector(`.${KITCHEN_MENU_LINK_CLASS}.current`),
   // the hashArray holds all of the hashes for different form sections
     hashArray = Array.prototype.map.call(document.querySelectorAll(`.${KITCHEN_MENU_LINK_CLASS}`), (el) => `#${el.href.split('#')[1]}`),
@@ -25,11 +26,8 @@ jsController['kitchen-new'] = function () {
   function handleButtonChanges(newHash) {
     var currHashIdx = hashArray.indexOf(newHash);
 
-    if (currHashIdx === 0) {
-      $prevButton.disabled = true;
-    } else {
-      $prevButton.disabled = false;
-    }
+    // disable previous button for first page
+    $prevButton.disabled = currHashIdx === 0;
 
     if (currHashIdx === hashArray.length - 1) {
       $nextButton.innerText = 'Finish';
@@ -38,13 +36,28 @@ jsController['kitchen-new'] = function () {
     }
   }
 
+  function addCompletionIcon($newMenuItem) {
+    var $requiredFields = $currentFieldset.querySelectorAll('[required="required"]'),
+      $filledFields = Array.prototype.filter.call($requiredFields, (el) => {
+        return String(el.value).trim();
+      });
+
+    if ($requiredFields.length === $filledFields.length) {
+      $newMenuItem.classList.remove(INCOMPLETE_LINK_CLASS);
+      $newMenuItem.classList.add(COMPLETED_LINK_CLASS);
+    } else {
+      $newMenuItem.classList.remove(COMPLETED_LINK_CLASS);
+      $newMenuItem.classList.add(INCOMPLETE_LINK_CLASS);
+    }
+  }
+
   // changes the current fieldset and menu item
   function changeCurrentFieldset(newHash) {
     const $newFieldset = document.querySelector(newHash);
     const $newMenuItem = document.querySelector(`.${MENU_LINK_CLASS}[href="${newHash}"]`);
-    const hashIdx = hashArray.indexOf(newHash);
 
     handleButtonChanges(newHash);
+    addCompletionIcon($currentMenuItem);
 
     $currentFieldset.classList.remove(CURRENT_FIELDSET_CLASS);
     $currentMenuItem.classList.remove(CURRENT_MENU_ITEM_CLASS);
@@ -61,7 +74,7 @@ jsController['kitchen-new'] = function () {
 
   // change shown fieldset on hashchange
   window.addEventListener('hashchange', () => {
-    let newHash = location.hash;
+    const newHash = location.hash;
 
     changeCurrentFieldset(newHash);
   });
@@ -69,16 +82,15 @@ jsController['kitchen-new'] = function () {
   // handle button clicks via delegation
   $buttonContainer.addEventListener('click', (evt) => {
     if (evt.target && (evt.target === $nextButton || evt.target === $prevButton)) {
-      let currIdx = hashArray.indexOf(location.hash);
+      const currIdx = hashArray.indexOf(location.hash);
 
-      console.log(currIdx, hashArray[currIdx]);
-      if(currIdx !== -1) {
+      if (currIdx !== -1) {
         evt.target === $nextButton ? location.hash = hashArray[currIdx + 1] : location.hash = hashArray[currIdx - 1];
       }
     }
   });
 
-//  sticky menu listener
+  //  sticky menu listener
   document.addEventListener('scroll', throttle(() => {
     if (!isStuck && $body.scrollTop >= navHeight) {
       $stickyMenu.classList.add(STICKY_MENU_CLASS);
