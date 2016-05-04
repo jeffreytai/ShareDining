0fd825245593ead10dbc860600cacd3e3aff134b
Mahir Shah <mahir@mahirshah.com>
2016-05-04 00:53:13 -0700

    fixed navigation styling and added make script

--- app/assets/javascripts/ModalController.js
+++ app/assets/javascripts/ModalController.js
@@ -8,52 +8,56 @@ export default class ModalShow {
   init() {
     const $body = document.querySelector('body');
     const $modal = document.querySelector(`.${this.modalClass}`);
-    const ESCAPE_KEY_CODE = 27;
-    let lastFocus = null;
-    const closeModal = () => {
-      $body.classList.remove(this.bodyModalClass);
-      lastFocus.focus(); // restore focus to the old element
-    };
 
-    // each link should open the modal by adding the bodyModalClass to body
-    this.modalLinks.forEach((link) => {
-      link.addEventListener('click', (evt) => {
-        // save the last focus
-        lastFocus = document.activeElement;
-        $body.classList.add(this.bodyModalClass);
+    // make sure the modal exists before continuing
+    if ($modal) {
+      const ESCAPE_KEY_CODE = 27;
+      let lastFocus = null;
+      const closeModal = () => {
+        $body.classList.remove(this.bodyModalClass);
+        lastFocus.focus(); // restore focus to the old element
+      };
 
-        // focus on the modal
-        $modal.setAttribute('tabindex', 0);
-        $modal.focus();
+      // each link should open the modal by adding the bodyModalClass to body
+      this.modalLinks.forEach((link) => {
+        link.addEventListener('click', (evt) => {
+          // save the last focus
+          lastFocus = document.activeElement;
+          $body.classList.add(this.bodyModalClass);
 
-        // stop prop so we don't close the modal when we bubble up to body
-        evt.stopPropagation();
+          // focus on the modal
+          $modal.setAttribute('tabindex', 0);
+          $modal.focus();
+
+          // stop prop so we don't close the modal when we bubble up to body
+          evt.stopPropagation();
+        });
       });
-    });
 
-    // make sure only modal elements are focusable
-    document.addEventListener('focus', (evt) => {
-      if ($body.classList.contains(this.bodyModalClass) && !$modal.contains(evt.target)) {
-        event.preventDefault();
-        $modal.focus();
-      }
-    }, true);
+      // make sure only modal elements are focusable
+      document.addEventListener('focus', (evt) => {
+        if ($body.classList.contains(this.bodyModalClass) && !$modal.contains(evt.target)) {
+          event.preventDefault();
+          $modal.focus();
+        }
+      }, true);
 
-    // a click outside the modal should close it
-    $body.addEventListener('click', () => {
-      if ($body.classList.contains(this.bodyModalClass)) {
-        closeModal();
-      }
-    });
+      // a click outside the modal should close it
+      $body.addEventListener('click', () => {
+        if ($body.classList.contains(this.bodyModalClass)) {
+          closeModal();
+        }
+      });
 
-    // inside the modal don't bubble up so we don't close it
-    $modal.addEventListener('click', (evt) => evt.stopPropagation());
+      // inside the modal don't bubble up so we don't close it
+      $modal.addEventListener('click', (evt) => evt.stopPropagation());
 
-    // handle modal close on escape
-    document.addEventListener('keyup', (evt) => {
-      if (evt.keyCode === ESCAPE_KEY_CODE && $body.classList.contains(this.bodyModalClass)) {
-        closeModal();
-      }
-    });
+      // handle modal close on escape
+      document.addEventListener('keyup', (evt) => {
+        if (evt.keyCode === ESCAPE_KEY_CODE && $body.classList.contains(this.bodyModalClass)) {
+          closeModal();
+        }
+      });
+    }
   }
 }
---
+++ make
@@ -0,0 +1,1 @@
+bundle install && rake db:seed && rake db:mongoid:create_indexes && rails server
