4e49096d88b2d1db567afb3c0624efcbbe4d8733
Jeffrey Tai <jeffreytai@ucla.edu>
2016-05-08 20:00:09 -0700

    adds reservation page

--- app/controllers/reservation_controller.rb
+++ app/controllers/reservation_controller.rb
@@ -1,2 +1,19 @@
 class ReservationController < ApplicationController
+
+  def new
+    @kitchen_id = params[:id]
+    puts @kitchen_id # doesn't work
+    @reservation = Reservation.new
+  end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+    @reservation.save
+  end
+
+  private
+    def reservation_params
+      params.require(:reservation).permit(:kitchen_id, :user_id, :start_time, :end_time)
+    end
+
 end
