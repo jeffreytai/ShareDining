9d6f7b401838fa449be62c0b73af6029dd9d1a8f
Jeffrey Tai <jeffreytai@ucla.edu>
2016-05-16 14:49:38 -0700

    cleaned up availability controller

--- app/controllers/availability_controller.rb
+++ app/controllers/availability_controller.rb
@@ -1,19 +1,6 @@
 class AvailabilityController < ApplicationController
 
   def create
-    @availability = Availability.new(availability_params)
-
-    puts "Sunday Availability: #{@availability.sunday_availability}"
-    puts "Monday Availability #{@availability.monday_availability}"
   end
-
-  private
-    def availability_params
-      params.require(:availability).permit(
-                                          :sunday_availability, :monday_availability, :tuesday_availability,
-                                          :wednesday_availability, :thursday_availability, :friday_availability,
-                                          :saturday_availability
-                                          )
-    end
 
 end
