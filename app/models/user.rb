ceea3c0dc49a6a28613b27085b9f53016e20ec92
rtphubeny <rtphubeny@yahoo.com>
2016-05-17 22:42:46 -0700

    messaging (create, reply, mark as read, trash, empty trash, sentbox)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,15 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  acts_as_messageable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :kitchens, dependent: :destroy
+
+  def mailboxer_email(object)
+    email
+  end
 end
